abd1632d7ae270358bb7817356f17d1080e359e0
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2024-12-02 20:05:41 +0200

    initial project setup

---
+++ onepace_file_organizer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+class OnePaceFileOrganizer:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    organizer = OnePaceFileOrganizer()
+    organizer.run()
