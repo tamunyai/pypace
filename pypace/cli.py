c6e97ef654f8991d05aa4478824f22ab8131ee8c
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2025-01-29 23:23:21 +0200

    feat: combined `OnePaceFileOrganizer` logic with the `Episode`-based approach

--- pypace/cli.py
+++ pypace/cli.py
@@ -1,21 +1,34 @@
 import argparse
+from pathlib import Path
 
-from .organizer import OnePaceFileOrganizer
+from colorama import Fore, Style
+
+from .organizer import organize_files, reset
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Organize One Pace files into sagas and arcs."
     )
+    parser.add_argument("directory", help="Directory containing One Pace files.")
+    parser.add_argument("--dry-run", action="store_true", help="Preview changes.")
     parser.add_argument(
-        "directory",
-        nargs="?",
-        default=".",
-        help="Directory containing One Pace files (default: current directory)",
+        "--reset",
+        action="store_true",
+        help="Move files back.",
     )
+
     args = parser.parse_args()
-    organizer = OnePaceFileOrganizer(args.directory)
-    organizer.organize_files()
+    directory = Path(args.directory).resolve()
+
+    print(f"{Fore.CYAN}Processing Directory: {Style.RESET_ALL}{directory}")
+
+    if args.reset:
+        print(f"{Fore.YELLOW}Resetting files...{Style.RESET_ALL}")
+        reset(directory, args.dry_run)
+    else:
+        print(f"{Fore.GREEN}Organizing files...{Style.RESET_ALL}")
+        organize_files(directory, args.dry_run)
 
 
 if __name__ == "__main__":
--- pypace/organizer.py
+++ pypace/organizer.py
@@ -1,202 +1,88 @@
 import logging
+import shutil
 from pathlib import Path
-from re import IGNORECASE, escape, search, sub
-from shutil import move
 
-# ANSI escape codes for colors
-RESET_COLOR = "\033[0m"
-RED = "\033[0;31m"
-GREEN = "\033[0;32m"
-YELLOW = "\033[0;33m"
-CYAN = "\033[0;36m"
+from colorama import Fore, Style, init
+
+from .data import SAGAS_WITH_ARCS
+from .models import Episode
+
+init(autoreset=True)
+
+logging.basicConfig(
+    format=f"{Fore.CYAN}%(levelname)s:{Style.RESET_ALL} %(message)s",
+    level=logging.INFO,
+)
 
 
-class OnePaceFileOrganizer:
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+def organize_files(directory: Path, dry_run: bool):
+    if not directory.is_dir():
+        logging.error(f"{Fore.RED}Invalid directory:{Style.RESET_ALL} {directory}")
+        return
+
+    # if not directory.exists():
+    #     logging.error(f"{Fore.RED}Invalid directory:{Style.RESET_ALL} {directory}")
+    #     return
+
+    arc_lookup = {
+        arc_name: arc
+        for saga in SAGAS_WITH_ARCS
+        for arc in saga.arcs
+        for arc_name in arc.all_names
+    }
+
+    for file in directory.rglob("*"):
+        if not (file.is_file() and "[One Pace]" in file.stem):
+            continue
+
+        try:
+            episode = Episode(filepath=file)
+
+        except ValueError as e:
+            logging.warning(f"{Fore.YELLOW}Skipping:{Style.RESET_ALL} {file} → {e}")
+            continue
+
+        arc = arc_lookup.get(episode.arc_name)
+        if arc:
+            arc.add_episode(episode)
+        else:
+            logging.warning(f"{Fore.YELLOW}No matching arc:{Style.RESET_ALL} {file}")
+            continue
+
+    files_moved_count = 0
+    for saga_index, saga in enumerate(SAGAS_WITH_ARCS, 1):
+        saga_folder = directory / f"{saga_index:02} - {saga.actual_name}"
+
+        for arc_index, arc in enumerate(saga.arcs, 1):
+            arc_folder = saga_folder / f"{saga_index:02}{arc_index} - {arc.actual_name}"
+
+            for episode in arc.episodes:
+                target_path = arc_folder / episode.filepath.name
+
+                if target_path.exists():
+                    continue
+
+                if not dry_run:
+                    arc_folder.mkdir(parents=True, exist_ok=True)
+                    shutil.move(episode.filepath, target_path)
+
+                logging.info(
+                    f"{Fore.GREEN}Moved:{Style.RESET_ALL} {episode.filepath} → {target_path}"
+                )
+                files_moved_count += 1
+
+    logging.info(
+        f"{Fore.MAGENTA}Total files moved:{Style.RESET_ALL} {files_moved_count}"
     )
 
-    def __init__(self, base_directory: str = "./") -> None:
-        """
-        Initializes the file organizer with the specified base directory and predefined saga and arc structure.
 
-        Args:
-            base_directory (str, optional): The directory where OnePace files are stored (default: current directory).
-        """
-        self.base_directory = Path(base_directory)
-        self.one_pace_files = self._get_one_pace_files()
-        self.sagas_with_arcs = self._get_sagas_with_arcs()
-        self.files_by_arc = self._organize_files_by_arc()
+def reset(directory: Path, dry_run: bool):
+    for file in directory.rglob("*"):
+        if not (file.is_file() and "[One Pace]" in file.stem):
+            continue
 
-    def _get_one_pace_files(self) -> list[Path]:
-        """
-        Retrieves all files in the base directory that are related to OnePace, identified by '[One Pace]' in the filename.
+        target_path = directory / file.name
+        if not dry_run:
+            shutil.move(file, target_path)
 
-        Returns:
-            list[Path] :A list of Path objects representing the OnePace files found in the base directory.
-        """
-        return [
-            file
-            for file in self.base_directory.iterdir()
-            if file.is_file() and "[One Pace]" in file.name
-        ]
-
-    def _get_sagas_with_arcs(self) -> dict[str, list[str]]:
-        """
-        Returns a dictionary mapping each saga to its corresponding arcs in the OnePiece series.
-
-        Returns:
-            dict[str, list[str]]: A dictionary where the keys are saga names (e.g., "East Blue Saga") and the values are lists of arc names.
-        """
-        return {
-            "East Blue Saga": [
-                "Romance Dawn Arc",
-                "Orange Town Arc",
-                "Syrup Village Arc",
-                "Baratie Arc",
-                "Arlong Park Arc",
-                "Loguetown Arc",
-            ],
-            "Arabasta Saga": [
-                "Reverse Mountain Arc",
-                "Whisky Peak Arc",
-                "Little Garden Arc",
-                "Drum Island Arc",
-                "Arabasta Arc",
-            ],
-            "Sky Island Saga": ["Jaya Arc", "Skypiea Arc"],
-            "Water Seven Saga": [
-                "Long Ring Long Land Arc",
-                "Water Seven Arc",
-                "Enies Lobby Arc",
-                "Post-Enies Lobby Arc",
-            ],
-            "Thriller Bark Saga": ["Thriller Bark Arc"],
-            "Summit War Saga": [
-                "Sabaody Archipelago Arc",
-                "Amazon Lily Arc",
-                "Impel Down Arc",
-                "Marineford Arc",
-                "Post-War Arc",
-            ],
-            "Fish-Man Island Saga": ["Return to Sabaody Arc", "Fish-Man Arc"],
-            "Dressrosa Saga": ["Punk Hazard Arc", "Dressrosa Arc"],
-            "Whole Cake Island Saga": ["Zou Arc", "Whole Cake Arc", "Levely Arc"],
-            "Wano Country Saga": ["Wano Country Arc"],
-            "Final Saga": ["EggHead Arc", "Elbaph Arc"],
-        }
-
-    def _organize_files_by_arc(self) -> dict[str, list[Path]]:
-        """
-        Organizes OnePace files by arc names, associating each arc with the relevant files in the base directory.
-
-        Returns:
-            dict[str, list[Path]]: A dictionary where keys are arc names (without "Arc" suffix) and values are lists of files for each arc.
-        """
-        files_by_arc = {}
-        for file in self.one_pace_files:
-            for arcs in self.sagas_with_arcs.values():
-                for arc_name in arcs:
-                    arc_name_without_suffix = sub(r" Arc$", "", arc_name)
-                    # arc_name_without_suffix = escape(arc_name_without_suffix)
-                    pattern = rf"\s{arc_name_without_suffix}\s"
-
-                    if not search(pattern, file.name, IGNORECASE):
-                        continue
-
-                    if arc_name_without_suffix not in files_by_arc:
-                        files_by_arc[arc_name_without_suffix] = []
-
-                    files_by_arc[arc_name_without_suffix].append(file)
-
-        return files_by_arc
-
-    def _create_directory_if_not_exists(self, directory: Path):
-        """
-        Creates a directory if it doesn't already exist.
-
-        Args:
-            directory (Path): The path to the directory to be created if it doesn't exist.
-        """
-        if not directory.exists():
-            logging.info(f"{CYAN}Creating directory: {directory}{RESET_COLOR}")
-            directory.mkdir(parents=True, exist_ok=True)
-
-    def _move_file(self, source_path: Path, destination_path: Path) -> bool:
-        """
-        Attempts to move a file from source to destination.
-
-        Args:
-            source_path (Path): The source file path.
-            destination_path (Path): The destination path where the file will be moved.
-
-        Returns:
-            bool: True if the file was moved successfully, False otherwise.
-        """
-        try:
-            if destination_path.exists():
-                logging.warning(
-                    f"{RED}Destination file {destination_path} already exists, skipping.{RESET_COLOR}"
-                )
-                return False
-
-            logging.info(f"{YELLOW}Moving: {source_path.name}{RESET_COLOR}")
-            move(source_path, destination_path)
-            logging.info(f"{GREEN}Moved: {destination_path}{RESET_COLOR}")
-            return True
-
-        except Exception as e:
-            logging.error(f"{RED}Error moving {source_path.name}: {e}{RESET_COLOR}")
-            return False
-
-    def _organize_arc(
-        self, saga_index: int, saga_name: str, arc_index: int, arc_name: str
-    ) -> int:
-        """
-        Organizes the files associated with a specific arc by moving them into the correct directory.
-
-        Args:
-            saga_index (int): The index of the saga in the overall series.
-            saga_name (int): The name of the saga (e.g., "East Blue Saga").
-            arc_index (int): The index of the arc within the saga.
-            arc_name (int): The name of the arc (e.g., "Romance Dawn Arc").
-
-        Returns:
-            int: The number of files successfully moved for this arc.
-        """
-        arc_name_without_suffix = sub(r" Arc$", "", arc_name)
-        saga_directory = self.base_directory / f"{saga_index:02} - {saga_name}"
-        arc_directory = saga_directory / f"{saga_index:02}{arc_index} - {arc_name}"
-
-        count = 0
-        files_for_arc = self.files_by_arc.get(arc_name_without_suffix, [])
-        for file in files_for_arc:
-            source_path = self.base_directory / file.name
-            destination_path = arc_directory / file.name
-
-            self._create_directory_if_not_exists(arc_directory)
-            if self._move_file(source_path, destination_path):
-                count += 1
-
-        return count
-
-    def organize_files(self):
-        """
-        Organizes OnePace files by their saga and arc by moving them into appropriate directories.
-        """
-        files_moved_count = 0
-        for saga_index, (saga_name, arcs) in enumerate(self.sagas_with_arcs.items(), 1):
-            for arc_index, arc_name in enumerate(arcs, 1):
-                files_moved_count += self._organize_arc(
-                    saga_index, saga_name, arc_index, arc_name
-                )
-
-        logging.info(f"Total files moved: {files_moved_count}")
-
-    def run(self):
-        """
-        Executes the file organization process.
-        """
-        logging.info(f"Starting file organization in '{self.base_directory.resolve()}'")
-        self.organize_files()
-        logging.info(f"File organization completed.")
+        logging.info(f"{Fore.BLUE}Reset:{Style.RESET_ALL} {file} → {target_path}")
